adf3e3fafb274c53966ed9c53b94f39110570508
empratyush <codelab@pratyush.dev>
2022-01-10 15:33:11 +0530

    Added few data classes
    Co-authored-by: quh4gko8 <88831734+quh4gko8@users.noreply.github.com>

---
+++ app/src/main/java/org/grapheneos/apps/client/item/SeamlessUpdateResponse.kt
@@ -0,0 +1,7 @@
+package org.grapheneos.apps.client.item
+
+data class SeamlessUpdateResponse(
+    val updatedSuccessfully : List<String> = emptyList(),
+    val failedToUpdate : List<String> = emptyList(),
+    val executedSuccessfully : Boolean = false
+)
